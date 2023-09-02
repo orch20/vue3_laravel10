e47bb11835fc7a950e2fe061441041e16c15317f
orch20 <94862153+orch20@users.noreply.github.com>
2023-09-02 15:08:49 +0300

    protected routes fron unauthourized users

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use App\Http\Controllers\Api\V1\CompleteTaskController;
 |
 */
 
-Route::prefix('v1')->group(function () {
+Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
     Route::apiResource('/tasks', TaskController::class);
     Route::patch('/tasks/{task}/complete', CompleteTaskController::class);
 });
