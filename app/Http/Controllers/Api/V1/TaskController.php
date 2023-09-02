3eec4101fb761812610e103c9744510fbcc2f2a4
orch20 <94862153+orch20@users.noreply.github.com>
2023-09-02 12:17:20 +0300

    delete task from database

--- app/Http/Controllers/Api/V1/TaskController.php
+++ app/Http/Controllers/Api/V1/TaskController.php
@@ -53,6 +53,7 @@ class TaskController extends Controller
      */
     public function destroy(Task $task)
     {
-        //
+        $task->delete();
+        return response()->noContent();
     }
 }
