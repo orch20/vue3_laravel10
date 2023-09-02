13cabe134cc40b19e469011475463d3c280f7e29
orch20 <94862153+orch20@users.noreply.github.com>
2023-09-02 16:22:03 +0300

    used Sanctum for Api token authentification

--- app/Http/Controllers/Api/Auth/LoginController.php
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
@@ -17,17 +17,17 @@ class LoginController extends Controller
      */
     public function __invoke(LoginRequest $request)
     {
-        // $user = User::where('email', $request->email)->first();
+        $user = User::where('email', $request->email)->first();
 
-        // if (!$user || !Hash::check($request->password, $user->password)) {
-        //     throw ValidationException::withMessages([
-        //         'email' => ['The credentials you entered are incorrect.']
-        //     ]);
-        // }
-        if (!auth()->attempt($request->only(['email', 'password']))) {
+        if (!$user || !Hash::check($request->password, $user->password)) {
             throw ValidationException::withMessages([
                 'email' => ['The credentials you entered are incorrect.']
             ]);
         }
+
+        return response()->json([
+            'user' => $user,
+            'token' => $user->createToken('laravel_api_token')->plainTextToken
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\TaskController;
+use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Controllers\Api\Auth\LogoutController;
+use App\Http\Controllers\Api\Auth\RegisterController;
 use App\Http\Controllers\Api\V1\CompleteTaskController;
 
 /*
@@ -21,6 +24,12 @@ Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
     Route::patch('/tasks/{task}/complete', CompleteTaskController::class);
 });
 // Route::apiResource('/v1/tasks', TaskController::class);
+
+Route::prefix('auth')->group(function () {
+    Route::post('/login', LoginController::class);
+    Route::post('/logout', LogoutController::class)->middleware('auth:sanctum');
+    Route::post('/register', RegisterController::class);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
